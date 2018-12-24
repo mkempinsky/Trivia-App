aae3c34e910f0ae01900b15ab44ade4b6f93a93d
Michele Kempinsky <mkempinsky@icloud.com>
2018-12-24 13:30:18 -0800

    build

---
+++ build/precache-manifest.2345a2056a4bdb1eccac8149d0c3e563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a99ce8b03289618e067",
+    "url": "/static/js/main.5a99ce8b.chunk.js"
+  },
+  {
+    "revision": "d2a72ad7e0b312479945",
+    "url": "/static/js/1.d2a72ad7.chunk.js"
+  },
+  {
+    "revision": "5a99ce8b03289618e067",
+    "url": "/static/css/main.723d9f42.chunk.css"
+  },
+  {
+    "revision": "f8c3b2ceb57a72b7688d72de48f14125",
+    "url": "/index.html"
+  }
+];
